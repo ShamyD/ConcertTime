1fe362deca63195a4fa8d12c625d7e67cafcc108
ShamyD <Seamus.D96@gmail.com>
2021-01-26 17:17:28 +0100

    satt igång

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print('hello')
+import requests
+
+'''artist_id = '379603'
+api_key = '1'
+searchstring = 'https://api.songkick.com/api/3.0/artists/' + artist_id + '/gigography.json?apikey=' + api_key
+print(searchstring)'''
+
+response = requests.get('https://opendata-download-metobs.smhi.se/api')
+print(response)
